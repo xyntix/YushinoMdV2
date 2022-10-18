4d1c37aee6859d91c948c124f41373aefea6f757
xyntix <61931770+xyntix@users.noreply.github.com>
2022-10-18 23:42:00 +0700

    Update exp-daftar.js

--- plugins/exp-daftar.js
+++ plugins/exp-daftar.js
@@ -3,49 +3,49 @@ import fetch from 'node-fetch'
 let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 
 let handler = async function (m, { text, usedPrefix, command }) {
-function pickRandom(list) {
+	function pickRandom(list) {
   return list[Math.floor(Math.random() * list.length)]
 }
-let namae = conn.getName(m.sender)
-const sections = [
-{
-title: "Select Your Age Here !",
-rows: [
-{title: "Random Years", rowId: '.daftar ' + namae + '.' + pickRandom(['30','29','28','27','26','25','24','23','22','21','20','19','18','17','16','15','14','13','12','11','10','9'])}
-]
-},
-{
-title: "O L D",
-rows: [
-{title: "30 Years", rowId: '.daftar ' + namae + '.30 '},
-{title: "29 Years", rowId: '.daftar ' + namae + '.29 '},
-{title: "28 Years", rowId: '.daftar ' + namae + '.28 '},
-{title: "27 Years", rowId: '.daftar ' + namae + '.27 '},
-{title: "26 Years", rowId: '.daftar ' + namae + '.26 '},
-{title: "25 Years", rowId: '.daftar ' + namae + '.25 '},
-{title: "24 Years", rowId: '.daftar ' + namae + '.24 '},
-{title: "23 Years", rowId: '.daftar ' + namae + '.23 '},
-{title: "22 Years", rowId: '.daftar ' + namae + '.22 '},
-{title: "21 Years", rowId: '.daftar ' + namae + '.21 '}
-]
-},
-{
-title: "Y O U N G",
-rows: [
-{title: "20 Years", rowId: '.daftar ' + namae + '.20 '},
-{title: "19 Years", rowId: '.daftar ' + namae + '.19 '},
-{title: "18 Years", rowId: '.daftar ' + namae + '.18 '},
-{title: "17 Years", rowId: '.daftar ' + namae + '.17 '},
-{title: "16 Years", rowId: '.daftar ' + namae + '.16 '},
-{title: "15 Years", rowId: '.daftar ' + namae + '.15 '},
-{title: "14 Years", rowId: '.daftar ' + namae + '.14 '},
-{title: "13 Years", rowId: '.daftar ' + namae + '.13 '},
-{title: "12 Years", rowId: '.daftar ' + namae + '.12 '},
-{title: "11 Years", rowId: '.daftar ' + namae + '.11 '},
-{title: "10 Years", rowId: '.daftar ' + namae + '.10 '},
-{title: "9 Years", rowId: '.daftar ' + namae + '.9 '}
-]
-},
+	let namae = conn.getName(m.sender)
+	const sections = [
+	{
+	title: "Select Your Age Here !",
+	rows: [
+	    {title: "Random Years", rowId: '.daftar ' + namae + '.' + pickRandom(['30','29','28','27','26','25','24','23','22','21','20','19','18','17','16','15','14','13','12','11','10','9'])}
+	]
+    },
+    {
+	title: "O L D",
+	rows: [
+	    {title: "30 Years", rowId: '.daftar ' + namae + '.30 '},
+	    {title: "29 Years", rowId: '.daftar ' + namae + '.29 '},
+	    {title: "28 Years", rowId: '.daftar ' + namae + '.28 '},
+	{title: "27 Years", rowId: '.daftar ' + namae + '.27 '},
+	{title: "26 Years", rowId: '.daftar ' + namae + '.26 '},
+	{title: "25 Years", rowId: '.daftar ' + namae + '.25 '},
+	{title: "24 Years", rowId: '.daftar ' + namae + '.24 '},
+	{title: "23 Years", rowId: '.daftar ' + namae + '.23 '},
+	{title: "22 Years", rowId: '.daftar ' + namae + '.22 '},
+	{title: "21 Years", rowId: '.daftar ' + namae + '.21 '}
+	]
+    },
+    {
+	title: "Y O U N G",
+	rows: [
+	    {title: "20 Years", rowId: '.daftar ' + namae + '.20 '},
+	    {title: "19 Years", rowId: '.daftar ' + namae + '.19 '},
+	    {title: "18 Years", rowId: '.daftar ' + namae + '.18 '},
+	{title: "17 Years", rowId: '.daftar ' + namae + '.17 '},
+	{title: "16 Years", rowId: '.daftar ' + namae + '.16 '},
+	{title: "15 Years", rowId: '.daftar ' + namae + '.15 '},
+	{title: "14 Years", rowId: '.daftar ' + namae + '.14 '},
+	{title: "13 Years", rowId: '.daftar ' + namae + '.13 '},
+	{title: "12 Years", rowId: '.daftar ' + namae + '.12 '},
+	{title: "11 Years", rowId: '.daftar ' + namae + '.11 '},
+	{title: "10 Years", rowId: '.daftar ' + namae + '.10 '},
+	{title: "9 Years", rowId: '.daftar ' + namae + '.9 '}
+	]
+    },
 ]
 
 const listMessage = {
@@ -105,7 +105,7 @@ const listMessage = {
 {'buttonId':'.donasi','buttonText':{'displayText':'ᴅᴏɴᴀsɪ'},'type':1}
 ],
 'headerType':6}
-await conn.sendMessage(m.chat,buttonMessage, { quoted:m})
+    await conn.sendMessage(m.chat,buttonMessage, { quoted:m})
 }
 handler.help = ['daftar', 'register'].map(v => v + ' <nama>.<umur>')
 handler.tags = ['xp']
